d20250fa871915488d452bed6c4a38933001cf50
Alsaef <saef.ratul@gmail.com>
2025-09-19 01:13:53 +0600

    4th commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SideBar children={children}></SideBar>
+        <SideBar>{children}</SideBar>
       </body>
     </html>
   );
